6536cb5f4193472d531ddec45a95f4832e7ca6b5
chenyongzhiaaron <262667641@qq.com>
2023-07-14 18:06:55 +0800

    提取请求参数的bug

--- common/action.py
+++ common/action.py
@@ -10,54 +10,54 @@
 import ast
 
 from common.crypto.encrypt_data import EncryptData
-from common.utils.load_and_execute_script import LoadScript
 from common.utils.decorators import singleton
+from common.utils.load_and_execute_script import LoadScript
 from common.validation.extractor import Extractor
 from common.validation.validator import Validator
 
 
 @singleton
 class Action(Extractor, LoadScript, Validator):
-	def __init__(self, initialize_data=None, bif_functions=None):
-		super().__init__()
-		self.encrypt = EncryptData()
-		self.__variables = {}
-		self.set_environments(initialize_data)
-		self.set_bif_fun(bif_functions)
-	
-	def execute_dynamic_code(self, item, code):
-		self.set_variables(item)
-		if code is not None:
-			try:
-				ast_obj = ast.parse(code, mode='exec')
-				compiled = compile(ast_obj, '<string>', 'exec')
-				exec(compiled, {"pm": self})
-			except SyntaxError as e:
-				error_message = f'Syntax error in dynamic code: {e}'
-				self._handle_error(error_message)
-			except Exception as e:
-				error_message = f"Error executing dynamic code: {e}"
-				self._handle_error(error_message)
-			finally:
-				return self.__variables
-		return item
-	
-	def _handle_error(self, error_message):
-		print(f'Error occurred: {error_message}')
-	
-	def set_variables(self, item):
-		self.__variables = item
-	
-	def update_variables(self, key, value):
-		self.__variables[f"{{{{{key}}}}}"] = value
-	
-	def get_variables(self, key=None):
-		"""获取依赖表 或 依赖表中key对应的值"""
-		return self.__variables if not key else self.__variables.get(key)
-	
-	def analysis_request(self, request_data, jp_dict, headers_crypto, headers, request_crypto):
-		# 提取请求参数信息
-		self.substitute_data(request_data, jp_dict=jp_dict)
-		# 请求头及body加密或者加签
-		headers, request_data = self.encrypt.encrypts(headers_crypto, headers, request_crypto, request_data)
-		return headers, request_data
+    def __init__(self, initialize_data=None, bif_functions=None):
+        super().__init__()
+        self.encrypt = EncryptData()
+        self.__variables = {}
+        self.set_environments(initialize_data)
+        self.set_bif_fun(bif_functions)
+    
+    def execute_dynamic_code(self, item, code):
+        self.set_variables(item)
+        if code is not None:
+            try:
+                ast_obj = ast.parse(code, mode='exec')
+                compiled = compile(ast_obj, '<string>', 'exec')
+                exec(compiled, {"pm": self})
+            except SyntaxError as e:
+                error_message = f'Syntax error in dynamic code: {e}'
+                self._handle_error(error_message)
+            except Exception as e:
+                error_message = f"Error executing dynamic code: {e}"
+                self._handle_error(error_message)
+            finally:
+                return self.__variables
+        return item
+    
+    def _handle_error(self, error_message):
+        print(f'Error occurred: {error_message}')
+    
+    def set_variables(self, item):
+        self.__variables = item
+    
+    def update_variables(self, key, value):
+        self.__variables[f"{{{{{key}}}}}"] = value
+    
+    def get_variables(self, key=None):
+        """获取依赖表 或 依赖表中key对应的值"""
+        return self.__variables if not key else self.__variables.get(key)
+    
+    def analysis_request(self, request_data, headers_crypto, headers, request_crypto, extract_request_data):
+        # 请求头及body加密或者加签
+        headers, request_data = self.encrypt.encrypts(headers_crypto, headers, request_crypto, request_data)
+        # 提取请求参数信息
+        self.substitute_data(request_data, jp_dict=extract_request_data)
+        return headers, request_data
--- common/validation/validator.py
+++ common/validation/validator.py
@@ -15,114 +15,112 @@ from common.validation.loaders import Loaders
 
 
 class Validator(Loaders):
-	"""
-	校验器
-	主要功能：
-		1、格式化校验变量
-		2、校验期望结果与实际结果与预期一致，并返回校验结果
-	"""
-	validate_variables_list = []
-	built_in_comparators = Loaders.load_built_in_comparators()
-	
-	def __init__(self):
-		super().__init__()
-	
-	def uniform_validate(self, validate_variables):
-		"""
-		统一格式化测试用例的验证变量validate
-		Args:
-			validate_variables: 参数格式 list、dict
-				示例：
-					[{"check":"result.user.name","comparator":"eq","expect":"chenyongzhi"}]
-					or {"check":"result.user.name","comparator":"eq","expect":"chenyongzhi"}
+    """
+    校验器
+    主要功能：
+        1、格式化校验变量
+        2、校验期望结果与实际结果与预期一致，并返回校验结果
+    """
+    validate_variables_list = []
+    built_in_comparators = Loaders.load_built_in_comparators()
+    
+    def __init__(self):
+        super().__init__()
+    
+    def uniform_validate(self, validate_variables):
+        """
+        统一格式化测试用例的验证变量validate
+        Args:
+            validate_variables: 参数格式 list、dict
+                示例：
+                    [{"check":"result.user.name","comparator":"eq","expect":"chenyongzhi"}]
+                    or {"check":"result.user.name","comparator":"eq","expect":"chenyongzhi"}
 
-		Returns: 返回数据格式 list
-				示例：
-					[{"check":"result.user.name","comparator":"eq","expect":"chenyongzhi"}]
+        Returns: 返回数据格式 list
+                示例：
+                    [{"check":"result.user.name","comparator":"eq","expect":"chenyongzhi"}]
 
-		"""
-		if isinstance(validate_variables, str):
-			validate_variables = json.loads(validate_variables)
-		if isinstance(validate_variables, list):
-			for item in validate_variables:
-				self.uniform_validate(item)
-		elif isinstance(validate_variables, dict):
-			if "check" in validate_variables.keys() and "expect" in validate_variables.keys():
-				# 如果验证mapping中不包含comparator时，默认为{"comparator": "eq"}
-				check_item = validate_variables.get("check")
-				expect_value = validate_variables.get("expect")
-				comparator = validate_variables.get("comparator", "eq")
-				self.validate_variables_list.append({
-					"check": check_item,
-					"expect": expect_value,
-					"comparator": comparator
-				})
-		else:
-			logger.error("参数格式错误！")
-	
-	def validate(self, resp_obj=None):
-		"""
-		校验期望结果与实际结果与预期一致
-		Args:
-			resp_obj: ResponseObject对象实例
+        """
+        if isinstance(validate_variables, str):
+            validate_variables = json.loads(validate_variables)
+        if isinstance(validate_variables, list):
+            for item in validate_variables:
+                self.uniform_validate(item)
+        elif isinstance(validate_variables, dict):
+            if "check" in validate_variables.keys() and "expect" in validate_variables.keys():
+                # 如果验证mapping中不包含comparator时，默认为{"comparator": "eq"}
+                check_item = validate_variables.get("check")
+                expect_value = validate_variables.get("expect")
+                comparator = validate_variables.get("comparator", "eq")
+                self.validate_variables_list.append({
+                    "check": check_item,
+                    "expect": expect_value,
+                    "comparator": comparator
+                })
+        else:
+            logger.error("参数格式错误！")
+    
+    def validate(self, resp_obj=None):
+        """
+        校验期望结果与实际结果与预期一致
+        Args:
+            resp_obj: ResponseObject对象实例
 
-		Returns:
+        Returns:
 
-		"""
-		
-		validate_pass = "PASS"
-		
-		# 记录校验失败的原因
-		failure_reason = []
-		for validate_variable in self.validate_variables_list:
-			check_item = validate_variable['check']
-			expect_value = validate_variable['expect']
-			comparator = validate_variable['comparator']
-			actual_value = Extractor.extract_value_by_jsonpath(resp_obj=resp_obj, expr=check_item)
-			try:
-				# 获取比较器
-				fun = self.built_in_comparators[comparator]
-				fun(actual_value=actual_value, expect_value=expect_value)
-			except (AssertionError, TypeError):
-				validate_pass = "FAIL"
-				failure_reason.append({
-					'检查项': check_item,
-					'期望值': expect_value,
-					'实际值': actual_value,
-					'断言方法': comparator_dict.get(comparator),
-				})
-		return validate_pass, failure_reason
-	
-	def run_validate(self, validate_variables, resp_obj=None):
-		"""
-		 统一格式化测试用例的验证变量validate，然后校验期望结果与实际结果与预期一致
-		Args:
-			validate_variables:参数格式 list、dict
-			resp_obj:ResponseObject对象实例
+        """
+        
+        validate_pass = "PASS"
+        
+        # 记录校验失败的原因
+        failure_reason = []
+        for validate_variable in self.validate_variables_list:
+            check_item = validate_variable['check']
+            expect_value = validate_variable['expect']
+            comparator = validate_variable['comparator']
+            actual_value = Extractor.extract_value_by_jsonpath(resp_obj=resp_obj, expr=check_item)
+            try:
+                # 获取比较器
+                fun = self.built_in_comparators[comparator]
+                fun(actual_value=actual_value, expect_value=expect_value)
+            except (AssertionError, TypeError):
+                validate_pass = "FAIL"
+                failure_reason.append({
+                    '检查项': check_item,
+                    '期望值': expect_value,
+                    '实际值': actual_value,
+                    '断言方法': comparator_dict.get(comparator),
+                })
+        return validate_pass, failure_reason
+    
+    def run_validate(self, validate_variables, resp_obj=None):
+        """
+         统一格式化测试用例的验证变量validate，然后校验期望结果与实际结果与预期一致
+        Args:
+            validate_variables:参数格式 list、dict
+            resp_obj:ResponseObject对象实例
 
-		Returns:返回校验结果
+        Returns:返回校验结果
 
-		"""
-		if not validate_variables:
-			return ""
-		# 清空校验变量
-		self.validate_variables_list.clear()
-		self.uniform_validate(validate_variables)
-		if not self.validate_variables_list:
-			raise "uniform_validate 执行失败，无法进行 validate 校验"
-		return self.validate(resp_obj)
+        """
+        if not validate_variables:
+            return ""
+        # 清空校验变量
+        self.validate_variables_list.clear()
+        self.uniform_validate(validate_variables)
+        if not self.validate_variables_list:
+            raise "uniform_validate 执行失败，无法进行 validate 校验"
+        return self.validate(resp_obj)
 
 
 if __name__ == '__main__':
-	# validate_variables1 = {"check": "$.result.user.name", "comparator": "eq", "expect": "chenyongzhi"}
-	validate_variables2 = [
-		{"check": "code", "comparator": "eq", "expect": "200"},
-		{"check": "result.user.name", "comparator": "eq", "expect": "chenyongzhi"}
-	]
-	resp_obj = {"code": "200", "result": {"user": {"name": "chenyongzhi"}}}
-	validator = Validator()
-	print(validator.run_validate(validate_variables2, resp_obj))
-# logger.info("---")
-# validator.run_validate(validate_variables2, resp_obj)
-# logger.info("---")
-# validator.run_validate(validate_variables2, resp_obj)
+    validate_variables1 = {"check": "$.result.user.name", "comparator": "eq", "expect": "chenyongzhi"}
+    validate_variables2 = [
+        {"check": "code", "comparator": "eq", "expect": "200"},
+        {"check": "result.user.name", "comparator": "eq", "expect": "chenyongzhi"}
+    ]
+    resp_obj = {"code": "200", "result": {"user": {"name": "chenyongzhi"}}}
+    validator = Validator()
+    print(validator.run_validate(validate_variables1, resp_obj))
+    print(validator.run_validate(validate_variables2, resp_obj))
+    print(validator.run_validate(validate_variables2, resp_obj))
