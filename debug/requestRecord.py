49bee29250b863aa3685aec3ebac091688772c9d
chenyongzhiaaron <262667641@qq.com>
2023-08-01 10:58:26 +0800

    测试浏览记录到csv

--- debug/requestRecord.py
+++ debug/requestRecord.py
@@ -3,8 +3,8 @@
 """
 @author: kira
 @contact: 262667641@qq.com
-@file: get_set.py
-@time: 2023/7/21 17:07
+@file: cap.py
+@time: 2023/7/28 16:12
 @desc:
 """
 import csv
@@ -12,46 +12,47 @@ import csv
 import mitmproxy.http
 
 
-class RequestRecorder:
+class CaptureInfoWriteFile:
     def __init__(self):
         self.records = []
-        self.url = None
-        self.body = None
-        self.params = None
-        self.method = None
-        self.headers = None
-    
+
     def request(self, flow: mitmproxy.http.HTTPFlow):
-        """获取请求数据"""
-        if "bimdc.bzlrobot.com" in flow.request.url:
-            self.url = flow.request.url
-            self.method = flow.request.method
-            self.headers = dict(flow.request.headers)
-            self.params = dict(flow.request.query or {})
-            self.body = flow.request.text
-    
+        self.url = flow.request.url  # 接口url
+        self.host = flow.request.host  # 域名
+        self.path = flow.request.path  # 接口地址
+        self.method = flow.request.method  # 请求方式
+        self.params = dict(flow.request.query or {})
+        self.content = flow.request.content
+        self.timestamp_start = flow.request.timestamp_start  # 请求开始时间戳
+        self.timestamp_end = flow.request.timestamp_end  # 请求结束时间戳
+        self.headers = flow.request.headers  # 请求头
+        self.body = flow.request.text  # 请求体
+
     def response(self, flow: mitmproxy.http.HTTPFlow):
-        if "bimdc.bzlrobot.com" in flow.request.url:
-            response_text = flow.response.text
-            response_status_code = flow.response.status_code
-            self.records.append({
-                "URL": self.url,
-                "Method": self.method,
-                "Headers": self.headers,
-                "Params": self.params,
-                "Body": self.body,
-                "Response Text": response_text,
-                "Status Code": response_status_code
-            })
-            """保存记录到 CSV 文件"""
-            with open(r"filtered_requests.csv", "a+", newline="", encoding="utf-8") as csvfile:
-                fieldnames = ["URL", "Method", "Headers", "Params", "Body", "Response Text", "Status Code"]
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                writer.writeheader()
-                for record in self.records:
-                    writer.writerow(record)
+        response_status_code = flow.response.status_code  # 响应状态码
+        response_text = flow.response.text  # 响应体
+        response_content = flow.response.content
+        response_timestamp_start = flow.response.timestamp_start  # 响应开始时间
+        response_timestamp_end = flow.response.timestamp_end  # 响应结束时间
+        response_header = flow.response.headers  # 响应头
+
+        self.records.append({
+            "URL": self.url,
+            "Method": self.method,
+            "Headers": self.headers,
+            "Params": self.params,
+            "Body": self.body,
+            "Response Text": response_text,
+            "Status Code": response_status_code
+        })
+
+        """保存记录到 CSV 文件"""
+        with open(r"filtered_requests.csv", "a+", newline="", encoding="utf-8") as csvfile:
+            fieldnames = ["URL", "Method", "Headers", "Params", "Body", "Response Text", "Status Code"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for record in self.records:
+                writer.writerow(record)
 
 
-addons = [
-    RequestRecorder()
-]
+addons = [CaptureInfoWriteFile()]
